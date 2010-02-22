97614f8fa184bd5af5f4d9d79ee5426403f57f14
Joshua Davey <josh@neotericdesign.com>
2010-02-22 17:47:12 -0600

    Rspec initial setup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,3 +26,9 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
+config.gem "rspec", :lib => false, :version => '~>1.3.0'
+config.gem "rspec-rails", :lib => false, :version => '~>1.3.2'
+config.gem "webrat", :lib => false, :version => '~>0.7.0'
+config.gem "cucumber", :lib => false, :version => '~>0.6.2'
+config.gem "cucumber-rails", :lib => false, :version => '~>0.2.4'
+config.gem "factory_girl", :version => '~>1.2.3'"
